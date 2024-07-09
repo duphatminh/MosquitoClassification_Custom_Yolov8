77d4a5804771f7823594c7ef7e30739941a501ab
DESKTOP-F7B8ATK <123456minhko@gmail.com>
2024-07-09 15:12:23 +0700

    Adding Dataset and train model with Yolov8-seg

---
+++ data/README.dataset.txt
@@ -0,0 +1,6 @@
+# moquitos > 2023-10-03 6:15pm
+https://universe.roboflow.com/randy-h-ej0mq/moquitos
+
+Provided by a Roboflow user
+License: MIT
+
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from ultralytics import YOLO
+if __name__ == '__main__':
+    from multiprocessing import freeze_support
+    freeze_support()
+    
+    from ultralytics import YOLO
 
-# Load a pretrained YOLO model (recommended for training)
-model = YOLO("yolov8n-seg.pt")
-
-# Train the model using the 'coco8.yaml' dataset for 3 epochs
-results = model.train(data="function.yaml", epochs=10, device="cuda:0")    
-
+    model = YOLO("yolov8n-seg.pt")
+    results = model.train(data="data/data.yaml", epochs=10, device="cuda:0")
