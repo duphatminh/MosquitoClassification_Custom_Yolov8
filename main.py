8288d1ef55fe6fdac5200b7a51d2e44889699c7d
duphatminh <duphatminhh@gmail.com>
2024-07-11 09:00:38 +0700

    Update main scripts

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-if __name__ == '__main__':
-    from multiprocessing import freeze_support
-    freeze_support()
-    
-    from ultralytics import YOLO
+from multiprocessing import freeze_support
+from ultralytics import YOLO
 
-    model_det = YOLO("yolov8n.pt")
-    results_det = model_det.train(data="data/data.yaml", epochs=50, device="cuda:0")
-    
-    # model_seg = YOLO("yolov8n-seg.pt")
-    # results_seg = model_seg.train(data="data/data.yaml", epochs=50, device="cuda:0")
+class Train_model_Yolov8:
+    def train_model_det(self):
+        model_det = YOLO("yolov8n.pt")
+        results_det = model_det.train(data="data/data.yaml", epochs=50, device="cuda:0")
+
+        return results_det
+
+    def train_model_seg(self):
+        model_seg = YOLO("yolov8n.pt")
+        results_seg = model_seg.train(data="data/data.yaml", epochs=50, device="cuda:0")
+
+        return results_seg
+
+if __name__ == '__main__':
+    freeze_support()
+    train = Train_model_Yolov8()
+
+    train.train_model_det()
+    #train.train_model_seg()
